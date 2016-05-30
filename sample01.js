4c251c1e06cff154e2e809388ff74a6335343ce5
viktor.v.lukashov@gmail.com <viktor.v.lukashov+github@gmail.com>
2016-05-30 11:54:22 +0000

    updates sample01.js

--- sample01.js
+++ sample01.js
@@ -1,1 +1,45 @@
-jsnc_eval_print('running as ' + gs.getUser().getName());
+//jsnc_eval_print('1: running as ' + gs.getUser().getName());
+
+var jsnc = new JasmineSNC(this);
+var jasmine = jsnc.jasmine;
+
+describe("Rhino 1.5R4 quirks", function() {
+  it("the jasmine.isSpy() method would not crash the js runtime", function() {
+    /*
+    in Rhino 1.5R4 the code 'undefined instanceof Object' crashes the runtime
+    add the following code to isSpy():
+    
+    if (j$.util.isUndefined(putativeSpy.and) || j$.util.isUndefined(putativeSpy.calls)) {
+      return false;
+    }
+    
+    */
+    var obj = {};
+    expect(jasmine.isSpy(obj)).toBe(false);
+  });
+  
+  it("an empty Array object evaluates to true", function() {
+    /*
+    In Rhino 1.5R4 the code below wrongly evaluates to true.
+    
+    var arr = [];
+    var result = (!arr);
+    
+    
+    Replace the following code in processNode():
+    
+      if (!node.children) {
+    
+    replace with
+    
+      if (!(typeof node.children === 'object')) {
+    
+    */
+    var arr = [];
+    var result = (!arr);
+    
+    expect(result).toBe(false);
+  });
+});
+
+jsnc.run();
